a6dda8c1cad60f3a4604bccc36da1c381e747308
David Brooks <dbrnz@users.noreply.github.com>
2026-02-17 15:55:01 +1300

    Include Pyodide's API in our exports.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@
 
 export { initialisePython, celldl2cellml } from './bg2cellml'
 export type { CellMLOutput } from './bg2cellml'
+
+// Export Pyodide's API
+
+export type { PyodideAPI } from 'pyodide'
